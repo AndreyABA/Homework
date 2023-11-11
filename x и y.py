bf5ca088cbe879e0b148d4a5f83ed2a7a5077eda
AndreyABA <150509320+AndreyABA@users.noreply.github.com>
2023-11-11 11:06:36 +0300

    Add files via upload

---
+++ x и y.py
@@ -0,0 +1,10 @@
+x=[]
+y=[]
+z=[]
+d=int(input('Введите количество точек '))
+for i in range(d):
+    a=[]
+    a.append(int(input('Введите переменную x ')))
+    a.append(int(input('Введите переменную y ')))
+    z.append(a)
+print('Координаты точки',z)
