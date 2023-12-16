3b8374786392dacb998e86a1b2eb878557a4d978
AndreyABA <150509320+AndreyABA@users.noreply.github.com>
2023-12-16 09:10:38 +0500

    Delete Блекджек.py
    1

--- Блекджек.py
+++
@@ -1,9 +0,0 @@
-a=[]
-c=int(input('Сколько карт? '))
-for i in range(c):
-    a.append(int(input('Введите достоинсвто карты ')))
-a=sum(a)
-if a<=21:
-    print('False')
-if a>21:
-    print('True')
